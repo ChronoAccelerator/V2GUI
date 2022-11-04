c6e5c63bd4a0b23425f7453a94dc63d85835d43b
Chronos <112827733+ChronoAccelerator@users.noreply.github.com>
2022-11-03 19:38:21 -0700

    Create Realstartupswag.lua

---
+++ Realstartupswag.lua
@@ -0,0 +1,14 @@
+local Library = loadstring(game:HttpGet("https://www.soggy-ware.cf/Libs/PromptGui.lua"))()
+Library:New({
+	Title = "Join Discord!",
+    Footer = "Join the discord for private V3 GUI!",
+    Text = "Proceed to join the discord? Yes = Yes, No = Take me to the GUI!",
+    Icon = "http://www.roblox.com/thumbs/asset.ashx?assetid=8412871039&x=100&y=100&format=png",
+    Yes = function()
+		setclipboard("discord.gg/RuF8M9hcCQ")
+    game:GetService("StarterGui"):SetCore("SendNotification", {Title = "Comet V3", Text = "Copied Discord!"})
+	end,
+	Cancel = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAccelerator/V2GUI/main/Startup.lua"))()
+	end
+})
